bc385471cf976b4d7f51656d4150aa552bda283d
JavierP <silver9ga@gmail.com>
2026-04-18 21:30:14 -0500

    blocks

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from textnode import *
+
 def main():
-    t = TextNode("Hello", TextType.BOLD_TEXT, "https://www.boot.dev")
+    t = TextNode("Hello", TextType.BOLD, "https://www.boot.dev")
     print(t)
+
 main()
 
